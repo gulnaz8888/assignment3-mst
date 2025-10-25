531272c98d1bea6c71bfb1008270763b6c927fbc
gulnaz8888 <242276@astanait.edu.kz>
2025-10-25 22:30:24 +0500

    Implement Edge class representing roads between districts with weight comparison for MST algorithms

--- src/main/java/org/example/Edge.java
+++ src/main/java/org/example/Edge.java
@@ -1,4 +1,51 @@
 package org.example;
+import java.util.Objects;
 
-public class Edge {
+public class Edge implements Comparable<Edge> {
+    private final int source;
+    private final int destination;
+    private final int weight;
+
+    public Edge(int source, int destination, int weight) {
+        this.source = source;
+        this.destination = destination;
+        this.weight = weight;
+    }
+
+    public int getSource() {
+        return source;
+    }
+
+    public int getDestination() {
+        return destination;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    @Override
+    public int compareTo(Edge other) {
+        return Integer.compare(this.weight, other.weight);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%d-%d, %d)", source, destination, weight);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Edge edge = (Edge) obj;
+        return source == edge.source &&
+                destination == edge.destination &&
+                weight == edge.weight;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(source, destination, weight);
+    }
 }
